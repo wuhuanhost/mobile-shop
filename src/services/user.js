ae8f5467cbb3d4f70b5e7d5e0fb23d389d4a6ead
lance0512 <lance0512@vip.qq.com>
2018-03-15 18:00:54 +0800

    添加了mockjs为项目添加模拟数据，优化了项目结构

--- src/services/user.js
+++ src/services/user.js
@@ -4,7 +4,7 @@ import goods from '../api/goods'
 function getUserInfoAndGoodsInfo () {
   return Promise.all([user.getUser(), goods.getGoods()]).then(result => {
     console.log(result)
-    result = null
+    // result = null
     return new Promise(function (resolve, reject) {
       if (!result) {
         reject(new Error('ERROR'))
